23b6e14ac10ef26f1f4aab1b38151d9be9cc3e23
ferCasadiego97 <fernanda.casadiego@mercadolibre.com>
2021-09-09 21:15:11 -0300

    ejercicios de javaScript terminados

--- PracticaJavaScriptBanco.js
+++ PracticaJavaScriptBanco.js
@@ -1,9 +1,98 @@
-function CuentaBancaria({ nombre, numero, apellido, saldo, movimientos }) {
-    this.nombre = nombre;
-    this.numero = numero;
-    this.apellido = apellido;
-    this.saldo = saldo;
-    this.movimientos = movimientos;
+const MOVIMIENTO = {
+    DEBITADO: "debitado",
+    ACREDITADO: "acreditado"
 }
 
-console.log({usuarios});
+function CuentaBancaria({nombre, numeroCuenta, apellido, saldo}) {
+    this.nombre = nombre;
+    this.__numeroCuenta = numeroCuenta;
+    this.apellido = apellido;
+    this.__saldo = saldo;
+    this.__movimientos = [];
+}
+
+CuentaBancaria.prototype.mostrarNombreTitular = function () {
+    return `${this.nombre} ${this.apellido}`;
+}
+
+CuentaBancaria.prototype.mostarSaldoCuenta = function () {
+    return `${this.__saldo} ars`;
+}
+
+CuentaBancaria.prototype.mostrarMovimientos = function () {
+    return this.__movimientos;
+}
+
+CuentaBancaria.prototype.mostrarNumeroCuenta = function () {
+    return this.__numeroCuenta;
+}
+
+CuentaBancaria.prototype.mostrarDetallesCuenta = function () {
+    return {
+        movimientos: this.mostrarMovimientos(),
+        titular: this.mostrarNombreTitular(),
+        saldo: this.mostarSaldoCuenta(),
+        cuenta: this.__numeroCuenta
+    };
+}
+
+
+CuentaBancaria.prototype.debitarSaldo = function (saldoADebitar) {
+    
+    if (saldoADebitar > this.__saldo) {
+        console.log(`No se puede debitar ${saldoADebitar} ars. Saldo Insuficiente`);
+        return;
+    }
+
+    const saldoAnterior = this.__saldo;
+    this.__saldo -= saldoADebitar;
+
+    this.__movimientos.push({
+        type: MOVIMIENTO.DEBITADO,
+        saldoAnterior,
+        saldoADebitar,
+        nuevoSaldo: this.__saldo,
+    });
+}
+
+CuentaBancaria.prototype.acreditarSaldo = function (saldoAAcreditar) {
+    
+    if (saldoAAcreditar <= 0) {
+        console.log(`No se puede acreditar el monto ${saldoAAcreditar} ars. Monto Invalido`);
+        return;
+    }
+
+    const saldoAnterior = this.__saldo;
+    this.__saldo += saldoAAcreditar;
+
+    this.__movimientos.push({
+        type: MOVIMIENTO.ACREDITADO,
+        saldoAnterior,
+            saldoAAcreditar,
+            nuevoSaldo: this.__saldo,
+    });
+}
+
+const accountDetails = { 
+    nombre: "Maria",
+    apellido: "Casadiego",
+    numeroCuenta: "012345684",
+    saldo: 1200
+}
+
+const cuenta = new CuentaBancaria(accountDetails);
+
+console.log("********");
+console.log(cuenta.mostrarDetallesCuenta());
+
+cuenta.debitarSaldo(300);
+
+console.log("********");
+console.log(cuenta.mostrarDetallesCuenta());
+
+cuenta.debitarSaldo(1200);
+
+cuenta.acreditarSaldo(3000);
+
+console.log("********");
+console.log(cuenta.mostrarDetallesCuenta());
